a9ac170a54edb4cd13bafd732073ad778ba83b40
ProgrammerColton <40554043+ProgrammerColton@users.noreply.github.com>
2018-06-25 16:54:45 -0700

    Create rbxapi.lua

---
+++ roblox/rbxapi.lua
@@ -0,0 +1,35 @@
+--[[
+ __                                   __                  
+|__)  _  _   _   _  _   _   _   _  _ /    _  | |_  _   _  
+|    |  (_) (_) |  (_| ||| ||| (- |  \__ (_) | |_ (_) | ) 
+            _/
+__________________________________________________________
+	
+	RbxAPI by ProgrammerColton
+	
+	A super simple proxy server that allows you to proxy requests to the ROBLOX API from a game server.
+	
+	It's nothing special and super simple to use.
+	I don't plan on anyone to use this, thus I'm not really gonna go into detail.
+	
+	Docs:
+	
+	GET /endpoint
+	ex: /users/1
+	
+	Here is a great documentation on the various ROBLOX apis. 
+	https://github.com/matthewdean/roblox-web-apis
+	
+--]]
+
+--// Code example:
+
+local settingsHandler = require(script.Settings);
+local rbxAPIHandler = require(script.RbxAPIModule);
+
+--// Set up the handler.
+local proxyServer = rbxAPIHandler:Setup(settingsHandler.url, settingsHandler.token);
+
+--// Send a get request;
+local getIdFromUsername = proxyServer:Get("/users/get-by-username?username=ProgrammerColton");
+print(getIdFromUsername);
