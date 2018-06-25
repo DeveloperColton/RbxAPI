4dbba7165038bed640f6467b104f4cbfcfde75b7
ProgrammerColton <40554043+ProgrammerColton@users.noreply.github.com>
2018-06-25 16:55:48 -0700

    Create settings.lua

---
+++ roblox/settings.lua
@@ -0,0 +1,7 @@
+local Settings = {
+	["token"] = "YOUR_TOKEN_HERE",
+	["url"] = "YOUR_URL_HERE"
+	--// Note: Remove the slash at the end of your url, it'll be automatically replaced.
+}
+
+return Settings
